ac332fa0ec750170b335d533f754566745feb9d8
Stefan <gajicstefan414@gmail.com>
2023-06-01 14:45:40 +0200

    Build a layout and responsiveness - build a layout of the first part of the calculator which are all input fields and tip percent buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,33 @@ module.exports = {
           "strong-cyan": "hsl(172, 67%, 45%)",
         },
         secondary: {
+          cyan: "hsl(172, 31%, 51%)",
+          "cyan-hover": "hsl(173, 61%, 77%)",
+
           "very-dark-cyan": "hsl(183, 100%, 15%)",
           "dark-grayish-cyan": "hsl(186, 14%, 43%)",
+
           "grayish-cyan": "hsl(184, 14%, 56%)",
           "light-grayish-cyan": "hsl(185, 41%, 84%)",
+
           "very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+
+          "shade-of-red": "hsl(12, 33%, 57%)",
+          "shade-of-gray": "hsl(203, 50%, 97%)",
         },
       },
       fontFamily: {
         mono: "'Space Mono', monospace",
+      },
+      spacing: {
+        4.25: "1.0625rem",
+        4.75: "1.1875rem",
+        21.75: "5.4375rem",
+      },
+      width: {
+        2.75: "0.6875rem",
+        3.25: "0.8125rem",
+        21.75: "5.4375rem",
       },
     },
   },
