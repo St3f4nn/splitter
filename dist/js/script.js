3146d7d35e69a609fcc5261e79ce31474ccc7646
Stefan <gajicstefan414@gmail.com>
2023-06-07 20:35:18 +0200

    Build a tip calculation functionality - calculate a tip by using a formula for tip calculation based on bill value with clicked button and display it on the user interface

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,8 +2,18 @@
 
 // Elements
 
+const billInputField = document.querySelector("#bill");
+const numberOfPeopleField = document.querySelector("#people-number");
+
 const tipBtns = document.querySelectorAll(".tip-btn");
 const tipInputField = document.querySelector(".tip-input");
+
+const tipAmountDisplay = document.querySelector("#tip-amount");
+const totalAmountDisplay = document.querySelector("#total-amount");
+
+const resetBtn = document.querySelector("#reset-btn");
+
+const warningMessages = document.querySelectorAll(".warning-message");
 
 // Functions
 
@@ -15,19 +25,57 @@ function removeSelectedClass() {
 
 // Main functionality
 
-/*
-
-Manipulating with 'selected' active class...
-
-On ANY clicked button with '%', it'll add a darker cyan background and text color. On the other side, when input field for percentages is clicked ('Custom'), it'll remove all colors from previously clicked buttons...
-
-*/
-
 for (let i = 0; i < tipBtns.length; i++) {
   tipBtns[i].addEventListener("click", function () {
     removeSelectedClass();
 
     tipBtns[i].classList.add("selected");
+
+    if (!billInputField.value && !numberOfPeopleField.value) {
+      billInputField.classList.add("empty-field");
+      numberOfPeopleField.classList.add("empty-field");
+
+      for (let i = 0; i < warningMessages.length; i++) {
+        warningMessages[i].classList.remove("hidden");
+      }
+    } else if (!billInputField.value) {
+      billInputField.classList.add("empty-field");
+      numberOfPeopleField.classList.remove("empty-field");
+
+      warningMessages[0].classList.remove("hidden");
+      warningMessages[1].classList.add("hidden");
+    } else if (!numberOfPeopleField.value) {
+      billInputField.classList.remove("empty-field");
+      numberOfPeopleField.classList.add("empty-field");
+
+      warningMessages[0].classList.add("hidden");
+      warningMessages[1].classList.remove("hidden");
+    } else {
+      billInputField.classList.remove("empty-field");
+      numberOfPeopleField.classList.remove("empty-field");
+
+      for (let i = 0; i < warningMessages.length; i++) {
+        warningMessages[i].classList.add("hidden");
+      }
+
+      const billValue = Number(billInputField.value);
+
+      const tipPercentage = tipBtns[i].textContent;
+      const tipPercentageNumber = Number(
+        tipPercentage.substring(0, tipPercentage.length - 1)
+      );
+
+      const tipAmountPerPerson = Number(
+        (billValue * (tipPercentageNumber / 100)).toFixed(2)
+      );
+
+      const totalPerPerson = billValue + tipAmountPerPerson;
+
+      tipAmountDisplay.textContent = tipAmountPerPerson;
+      totalAmountDisplay.textContent = totalPerPerson;
+
+      resetBtn.classList.remove("unclickable");
+    }
   });
 }
 
