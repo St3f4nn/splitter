3a4e71111b47bf858f99f7e81c4fce27ab9e91ea
Stefan <gajicstefan414@gmail.com>
2023-06-08 11:11:45 +0200

    Build a tip calculation functionality - add keyup event on tip input field (Custom) which does exact same thing as the previous commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -23,6 +23,19 @@ function removeSelectedClass() {
   }
 }
 
+function calculateTip(tip) {
+  const billValue = Number(billInputField.value);
+
+  const tipAmountPerPerson = Number((billValue * (tip / 100)).toFixed(2));
+
+  const totalPerPerson = (billValue + tipAmountPerPerson).toFixed(2);
+
+  tipAmountDisplay.textContent = tipAmountPerPerson;
+  totalAmountDisplay.textContent = totalPerPerson;
+
+  resetBtn.classList.remove("unclickable");
+}
+
 // Main functionality
 
 for (let i = 0; i < tipBtns.length; i++) {
@@ -58,25 +71,65 @@ for (let i = 0; i < tipBtns.length; i++) {
         warningMessages[i].classList.add("hidden");
       }
 
-      const billValue = Number(billInputField.value);
-
       const tipPercentage = tipBtns[i].textContent;
       const tipPercentageNumber = Number(
         tipPercentage.substring(0, tipPercentage.length - 1)
       );
 
-      const tipAmountPerPerson = Number(
-        (billValue * (tipPercentageNumber / 100)).toFixed(2)
-      );
-
-      const totalPerPerson = billValue + tipAmountPerPerson;
-
-      tipAmountDisplay.textContent = tipAmountPerPerson;
-      totalAmountDisplay.textContent = totalPerPerson;
-
-      resetBtn.classList.remove("unclickable");
+      calculateTip(tipPercentageNumber);
     }
   });
 }
 
 tipInputField.addEventListener("click", removeSelectedClass);
+tipInputField.addEventListener("keyup", function (e) {
+  let eventCode = e.key;
+
+  switch (eventCode) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+    case "Backspace":
+      if (!billInputField.value && !numberOfPeopleField.value) {
+        billInputField.classList.add("empty-field");
+        numberOfPeopleField.classList.add("empty-field");
+
+        for (let i = 0; i < warningMessages.length; i++) {
+          warningMessages[i].classList.remove("hidden");
+        }
+      } else if (!billInputField.value) {
+        billInputField.classList.add("empty-field");
+        numberOfPeopleField.classList.remove("empty-field");
+
+        warningMessages[0].classList.remove("hidden");
+        warningMessages[1].classList.add("hidden");
+      } else if (!numberOfPeopleField.value) {
+        billInputField.classList.remove("empty-field");
+        numberOfPeopleField.classList.add("empty-field");
+
+        warningMessages[0].classList.add("hidden");
+        warningMessages[1].classList.remove("hidden");
+      } else {
+        billInputField.classList.remove("empty-field");
+        numberOfPeopleField.classList.remove("empty-field");
+
+        for (let i = 0; i < warningMessages.length; i++) {
+          warningMessages[i].classList.add("hidden");
+        }
+
+        const tipPercentage = tipInputField.value;
+
+        calculateTip(tipPercentage);
+      }
+      break;
+    default:
+      return;
+  }
+});
